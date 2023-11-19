989185a851462be410c74165063cda3ee5761325
Dazannah <beattius@gmail.com>
2023-11-19 12:44:06 +0100

    4 class that implements Operation

--- index.php
+++ index.php
@@ -5,5 +5,30 @@
     public function doOperation($number1, $number2)
   }
 
+  class Addition implements Operation{
+    public function doOperation($number1, $number2){
+      return $number1 + $number2;
+    }
+  }
+
+  class Subtraction implements Operation{
+    public function doOperation($number1, $number2){
+      return $number1 - $number2;
+    }
+  }
+
+  class Multiplication implements Operation{
+    public function doOperation($number1, $number2){
+      return $number1 * $number2;
+    }
+  }
+
+  class Division implements Operation{
+    public function doOperation($number1, $number2){
+      return $number1 * $number2;
+    }
+  }
+
   
+
 ?>
